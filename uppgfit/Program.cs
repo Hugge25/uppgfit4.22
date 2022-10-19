b14ee087b21dab3806b3a0671b696a70b4355a9d
Admin <Admin@TEB123014>
2022-10-19 11:39:01 +0200

    Add project files.

---
+++ uppgfit/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Uppgfit4._22
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            for(int i = 10; i <=30; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(100);
+            }
+            for(int j = 200; j >= 180; j--)
+            {
+                Console.WriteLine(j);
+                Thread.Sleep(100);
+            }
+            for (int y = 1000; y <= 1400; y+=50)
+            {
+                Console.WriteLine(y);
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
